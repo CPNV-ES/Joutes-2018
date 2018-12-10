8a05090f8f538199362981a42095129f42a8e2c3
Kevin Jordil <kevin.jordil@cpnv.ch>
2018-12-10 08:45:06 +0100

    Add login route

--- routes/api.php
+++ routes/api.php
@@ -11,48 +11,50 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
 
-    $api->group(['middleware'=>App\Http\Middleware\AddCorsHeaders::class], function($api) {
+    $api->group(['middleware' => App\Http\Middleware\AddCorsHeaders::class], function ($api) {
 
-        $api->resource('events', 'App\Http\Controllers\EventController', [ 'only' => [
+        $api->resource('events', 'App\Http\Controllers\EventController', ['only' => [
             'index', 'show'
         ]]);
 
-        $api->resource('events.tournaments', 'App\Http\Controllers\EventTournamentController', [ 'only' => [
+        $api->resource('events.tournaments', 'App\Http\Controllers\EventTournamentController', ['only' => [
             'index', 'show'
         ]]);
 
-        $api->resource('events.tournaments.pools', 'App\Http\Controllers\API\EventTournamentPoolController', [ 'only' => [
+        $api->resource('events.tournaments.pools', 'App\Http\Controllers\API\EventTournamentPoolController', ['only' => [
             'index', 'show'
         ]]);
 
-        $api->resource('events.teams', 'App\Http\Controllers\API\EventTeamController', [ 'only' => [
+        $api->resource('events.teams', 'App\Http\Controllers\API\EventTeamController', ['only' => [
             'index', 'show'
         ]]);
 
-        $api->resource('events.participants', 'App\Http\Controllers\API\EventParticipantController', [ 'only' => [
+        $api->resource('events.participants', 'App\Http\Controllers\API\EventParticipantController', ['only' => [
             'index', 'show'
         ]]);
 
-        $api->resource('participants.notifications', 'App\Http\Controllers\API\ParticipantNotificationController', [ 'only' => [
+        $api->resource('participants.notifications', 'App\Http\Controllers\API\ParticipantNotificationController', ['only' => [
             'index'
         ]]);
-        
-        $api->resource('notifications', 'App\Http\Controllers\NotificationController', [ 'only' => [
+
+        $api->resource('notifications', 'App\Http\Controllers\NotificationController', ['only' => [
             'update'
         ]]);
-        
-        $api->resource('tournaments.schedule', 'App\Http\Controllers\API\ScheduleController', [ 'only' => [
+
+        $api->resource('tournaments.schedule', 'App\Http\Controllers\API\ScheduleController', ['only' => [
             'index'
         ]]);
 
-       $api->resource('profil', 'App\Http\Controllers\API\ProfilController', [ 'only' => [
-           'index'
-       ]]);
+        $api->resource('profil', 'App\Http\Controllers\API\ProfilController', ['only' => [
+            'index'
+        ]]);
+
+        $api->resource('login', 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller@login');
     });
 });
