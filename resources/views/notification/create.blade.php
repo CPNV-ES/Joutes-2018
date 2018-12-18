bd01b7aa677392cccafb9589a884f085fff14735
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-12-18 14:11:12 +0100

    Add container for view

--- resources/views/notification/create.blade.php
+++ resources/views/notification/create.blade.php
@@ -2,19 +2,21 @@
 @extends('layout')
 
 @section('content')
-<h1>Bonjour Maitre Dafflon</h1>
+<div class="container">
+  <h1>Bonjour Maitre Dafflon</h1>
 
-<form  action="/notifications" method="POST">
-{{ csrf_field() }}
-    <label for="title">Titre de la notification</label>
-    <input type="text" id="title" name="title" placeholder="Ex: Retard du tournoi">
-    <label for="description">description</label>
-    <input type="text" name="description" id="description">
-    <select name="team" id="team">
-    @foreach ($teams as $team)
-      <option value="{{$team->id}}">{{$team->name}}</option>
-    @endforeach
-    </select>
-    <input type="submit">
-</form>
+  <form  action="/notifications" method="POST">
+  {{ csrf_field() }}
+      <label for="title">Titre de la notification</label>
+      <input type="text" id="title" name="title" placeholder="Ex: Retard du tournoi">
+      <label for="description">description</label>
+      <input type="text" name="description" id="description">
+      <select name="team" id="team">
+      @foreach ($teams as $team)
+        <option value="{{$team->id}}">{{$team->name}}</option>
+      @endforeach
+      </select>
+      <input type="submit">
+  </form>
+</div>
 @stop
