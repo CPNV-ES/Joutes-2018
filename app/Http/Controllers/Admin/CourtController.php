8677a2be219844d75495061695b5bd2842e8881a
Senistan <senistan.jegarajasingam@cpnv.ch>
2019-01-24 11:30:43 +0100

    Update CourtController.php

--- app/Http/Controllers/Admin/CourtController.php
+++ app/Http/Controllers/Admin/CourtController.php
@@ -144,7 +144,7 @@ class CourtController extends Controller
         // create the validation rules
         $rules = array(
             'name' => 'required|min:1|max:20',
-            'acronym' => 'required|min:1|max:3',
+            'acronym' => 'required|min:1|max:5',
             'sport' => 'required',
         );
 
