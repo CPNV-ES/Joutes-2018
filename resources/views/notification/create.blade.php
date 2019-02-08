5a7821429469b6274c0e12d3faec48f27082cde1
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-02-08 11:54:48 +0100

    Update create notification blade

--- resources/views/notification/create.blade.php
+++ resources/views/notification/create.blade.php
@@ -3,20 +3,29 @@
 
 @section('content')
 <div class="container">
-  <h1>Bonjour Maitre Dafflon</h1>
+  <h1>Créér une notification</h1>
 
-  <form  action="/notifications" method="POST">
+  {{ Form::open(array('route' => 'notification.store')) }}
   {{ csrf_field() }}
-      <label for="title">Titre de la notification</label>
-      <input type="text" id="title" name="title" placeholder="Ex: Retard du tournoi">
-      <label for="description">description</label>
-      <input type="text" name="description" id="description">
-      <select name="team" id="team">
-      @foreach ($teams as $team)
-        <option value="{{$team->id}}">{{$team->name}}</option>
-      @endforeach
-      </select>
-      <input type="submit">
-  </form>
+    <div class="form-group">
+      {{ Form::label('title', 'Titre de la notification') }}
+      {{ Form::text('title', null, array('class' => 'form-control', 'placeholder' => 'Attente, Retard')) }}
+    </div>
+    <div class="form-group">
+      {{ Form::label('description', 'Description') }}
+      {{ Form::text('description', null, array('class' => 'form-control', 'placeholder' => 'Equipe 2 attendue, Retard au terrain de volley')) }}
+    </div>
+    <div class="form-group">
+      {{ Form::label('teams', 'Equipes') }}
+      <select class="form-control" name="team" id="team">
+        @foreach ($teams as $team)
+          <option value="{{$team->id}}">{{$team->name}}</option>
+        @endforeach
+        </select>
+    </div>
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit">
+    </div>    
+  {{ Form::close() }}
 </div>
 @stop
