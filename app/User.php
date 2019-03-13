429efacb7ea193e9b923fb34e921f5ad3ca5f9a1
Senistan <senistan.jegarajasingam@cpnv.ch>
2019-03-13 13:52:31 +0100

    Change the driver's database for "database"

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Model implements Authenticatable
 {   
