196257c6354a662fb0a11b95e94966e95f8d8b7f
Jérémy <jeremy.gfeller@hotmail.com>
2019-03-28 13:51:14 +0100

    Add a type of tournament
    It works but there is some errors to correct

--- app/Http/Controllers/Admin/TournamentPoolController.php
+++ app/Http/Controllers/Admin/TournamentPoolController.php
@@ -32,7 +32,8 @@ class TournamentPoolController extends Controller
      */
     public function store(Request $request)
     {
-        // TO DO : Store in DB
+        // pool, start_hour, time_match, nb_team
+        $this->elimination_direct($request->start_hour, $request->pool);
     }
 
     /**
@@ -58,5 +59,98 @@ class TournamentPoolController extends Controller
 
         // ajax will go into success
         return "{}";
+    }
+
+    private function elimination_direct($start_hour, $pool_id)
+    {
+        echo "================================================================================================================\n";
+        echo "Tennis\n";
+        echo "================================================================================================================\n";
+        $bv = \App\Tournament::where('name', 'like', '%Tennis%')->where('start_date', '>=', '2019-01-01')->first();
+        if (!$bv)
+        {
+            echo "Le tournoi de tennis n'existe pas\n";
+            return;
+        }
+        $tournamentid = $bv->id;
+        $sportid = $bv->sport_id;
+
+        $teams = \App\Team::where('tournament_id', '=', $tournamentid)->get();
+
+        echo "Tournoi #$tournamentid, " . $teams->count() . " équipes inscrites\n";
+        echo "Elimination directe\n";
+
+        for ($stage = 1; $stage <= 5; $stage++)
+        {
+            echo "Tour $stage, ".pow (2,5-$stage)." matches ";
+            for ($pooln = 1; $pooln <= pow (2, 5-$stage); $pooln++)
+            {
+                echo ".";
+                $pool = new \App\Pool([
+                    'tournament_id' => $tournamentid,
+                    'start_time' => $start_hour,
+                    'end_time' => '16:00',
+                    'poolName' => "Game $stage$pooln",
+                    'mode_id' => $pool_id,
+                    'game_type_id' => 1,
+                    'poolSize' => 2,
+                    'stage' => $stage,
+                    'isFinished' => 0
+                ]);
+                $pool->save();
+                if (!isset($firstpoolStage1)) $firstpoolStage1 = $pool->id; // we'll need that to put teams into pools
+            }
+            echo "\n";
+        }
+
+        echo "OK\nInscription des équipes ... ";
+
+        $pooln = 0;
+        foreach ($teams as $team)
+        {
+            $contender = new \App\Contender([
+                'pool_id' => $firstpoolStage1+intdiv($pooln++,2),
+                'team_id' => $team->id
+            ]);
+            $contender->save();
+            if (!isset($firstcontender)) $firstcontender = $contender->id; // we'll need that to put teams into pools
+        }
+
+        echo "OK\nLiens entre poules ... ";
+        $nbpoolstoadd = 8;
+        $pooldelta = 16;
+        $poolfromdelta = 0;
+
+        while ($nbpoolstoadd > 0)
+        {
+            for ($pooln = 0; $pooln < $nbpoolstoadd; $pooln++)
+            {
+                $contender = new \App\Contender([
+                    'pool_id' => $firstpoolStage1+$pooldelta+$pooln,
+                    'pool_from_id' => $firstpoolStage1+$poolfromdelta+$pooln*2,
+                    'rank_in_pool' => 1
+                ]);
+                $contender->save();
+                $contender = new \App\Contender([
+                    'pool_id' => $firstpoolStage1+$pooldelta+$pooln,
+                    'pool_from_id' => $firstpoolStage1+$poolfromdelta+$pooln*2+1,
+                    'rank_in_pool' => 1
+                ]);
+                $contender->save();
+            }
+            $poolfromdelta = $pooldelta;
+            $pooldelta += $nbpoolstoadd;
+            $nbpoolstoadd = intdiv($nbpoolstoadd,2);
+        }
+        
+        $firstcourt = \App\Court::where('sport_id', '=', $sportid)->first()->id;
+
+        echo "OK\nMatches ... ";
+
+        for ($matchnumber = 0; $matchnumber < 31; $matchnumber++)
+            (new \App\Game(['date' => '2017-06-27', 'start_time' => '09:30', 'contender1_id' => $firstcontender+$matchnumber*2, 'contender2_id' => $firstcontender+$matchnumber*2 + 1, 'court_id' => $firstcourt]))->save();
+
+        echo "OK\nTerminé.\n\n";
+
     }
 }
