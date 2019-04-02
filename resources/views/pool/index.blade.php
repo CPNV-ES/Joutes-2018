7a07ddd340b1c266e7767f07492dcf0780218498
Jérémy <jeremy.gfeller@cpnv.ch>
2019-04-02 16:56:23 +0200

    Show all the team for a tournament

--- resources/views/pool/index.blade.php
+++ resources/views/pool/index.blade.php
@@ -4,49 +4,52 @@
 
 @section('content')
 
-<div class="container">
+	<div class="container">
 		<a href=""><i class="fa fa-4x fa-arrow-circle-left return" aria-hidden="true"></i></a>	
 		<h1>Créer une pool</h1>
 
 		@if ($errors->any() || isset($customError))
 			<div class="alert alert-danger">
 				@if ($errors->any())
-		            @foreach ($errors->all() as $error)
-		                {{ $error }}<br>
-		            @endforeach
-		        @endif
-		        @if (isset($customError))
-		        	{{ $customError}}
-		        @endif
-	        </div>
-        @endif
+					@foreach ($errors->all() as $error)
+						{{ $error }}<br>
+					@endforeach
+				@endif
+				@if (isset($customError))
+					{{ $customError}}
+				@endif
+			</div>
+		@endif
 
 		{{ Form::open(array('url' => route('tournaments.pools.store', request()->route()->parameters), 'method' => 'post', 'id' => 'formPool')) }}
-
-		<div class="form-group">
-			{{ Form::label('pool', 'Type de pool') }}
-			<select class="form-control allSameStyle" id="pool" name="pool">
-				<option selected>Sélectionner</option>
-				@foreach($pools as $pool)
-					<option value='{{ $pool->id }}'>{{ $pool->mode_description }}</option>
-				@endforeach
-			</select>
+		<div class="col-lg-6">
+			<table id="tournament-teams-table" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+				<thead>
+					<tr>
+						<th>Liste des équipes participantes</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($tournament->teams as $team)
+						@if(count($tournament->teams) > 0 && $team->isValid())
+							<tr>
+								@if(Auth::check() && Auth::user()->role == 'administrator')
+									<td class="clickable" data-id="{{$team->id}}">{{$team->name}}</td>
+								@endif
+								<td>{{$team->participants()->count()}}</td>
+								<td><i class="{{ $team->isComplete() ? 'fa fa-check' : 'fa fa-close' }}" aria-hidden="true"></i></td>
+								<td><i class="{{ $team->isValid() ? 'fa fa-check' : 'fa fa-close' }}" aria-hidden="true"></i></td>
+							</tr>
+						@endif
+					@endforeach
+					@if(count($tournament->teams) > 0 && $team->isValid())
+						<tr>
+							<td>Aucune équipe pour l'instant ...</td>
+						</tr>
+					@endif
+				</tbody>
+			</table>
 		</div>
-		<div class="form-group">
-			{{ Form::label('start_hour', 'Heure de début') }}
-			{{ Form::text('start_hour', null, array('class' => 'form-control')) }}
-		</div>
-		<div class="form-group">
-			{{ Form::label('time_match', 'Temps par match') }}
-			{{ Form::text('time_match', null, array('class' => 'form-control')) }}
-		</div>
-		<div class="form-group">
-			{{ Form::label('nb_team', 'Nombre d\'équipe') }}
-			{{ Form::text('nb_team', null, array('class' => 'form-control')) }}
-		</div>	
-
-		<div class="send">{{ Form::button('Créer', array('class' => 'btn btn-success formSend')) }}</div>
-
 		{{ Form::close() }}
 
 	</div>
