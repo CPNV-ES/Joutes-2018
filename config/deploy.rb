515c2a2e310dd5ca14631fd9b3b3e53a8be38bd5
GERMANN Niels <Niels.GERMANN@cpnv.ch>
2019-05-07 16:23:11 +0200

    Modifs légères sur la planif. initiale. Disponible en .pdf.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10"
+lock "~> 3.10.1"
 
 set :application, "my_app_name"
 set :repo_url, "git@example.com:me/my_repo.git"
