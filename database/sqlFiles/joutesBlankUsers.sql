1856ea2b74cbefc84f99690abee985c430f72ae7
QuentinRo <quentin.rossier@cpnv.ch>
2019-06-24 14:48:41 +0200

    Rename the file

---
+++ database/sqlFiles/joutesBlankUsers.sql
@@ -0,0 +1,66 @@
+
+-- All password is set to "Admin"
+INSERT INTO joutes.users (first_name, last_name ,username, password, email, role)
+VALUES ('admin', 'admin' , 'admin','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'admin@admin.com', 'administrator'),
+('writer', 'writer' , 'writer','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'writer@writer.com', 'writer'),
+('Ahmed', 'Casey' , 'Ahmed','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Ahmed@Casey.com', 'participant'),
+('Chester', 'Day' , 'Chester','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Ahmed@Day.com', 'participant'),
+('test', 'test' , 'test','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'test@test.com', 'participant'),
+('Riley', 'Garison' , 'Riley','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Riley@test.com', 'participant'),
+('Duncan', 'Roy' , 'Duncan','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Duncan@test.com', 'participant'),
+('Remedios', 'Black' , 'Remedios','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Remedios@test.com', 'participant'),
+('Mark', 'Molina' , 'Mark','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Mark@test.com', 'participant'),
+('Dana', 'Justice' , 'Dana','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Dana@test.com', 'participant');
+
+
+INSERT INTO joutes.users (first_name, last_name ,username, password, email, role)
+VALUES ('Allegra', 'Waller' , 'Allegra','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Allegra@Allegra.com', 'participant'),
+('Emery', 'Copeland' , 'Emery','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Emery@Casey.com', 'participant'),
+('Illana', 'Mcgowan' , 'Illana','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Illana@Day.com', 'participant'),
+('Magee', 'Bauer' , 'Magee','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Magee@test.com', 'participant'),
+('Patricia', 'Briggs' , 'Patricia','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Patricia@test.com', 'participant'),
+('Samuel', 'Meyers' , 'Samuel','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Samuel@test.com', 'participant'),
+('Nelle', 'Holcomb' , 'Nelle','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Nelle@test.com', 'participant'),
+('Shay', 'David' , 'Shay','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Shay@test.com', 'participant'),
+('Brendan', 'Macdonald' , 'Brendan','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Brendan@test.com', 'participant'),
+('Kai', 'Quinn' , 'Kai','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Kai@test.com', 'participant');
+
+
+INSERT INTO joutes.users (first_name, last_name ,username, password, email, role)
+VALUES ('Justin', 'Jones' , 'Justin','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Justin@Justin.com', 'participant'),
+('Erich', 'Sheperd' , 'Erich','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Erich@Casey.com', 'participant'),
+('Joseph', 'Compton' , 'Joseph','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Joseph@Day.com', 'participant'),
+('Moses', 'Pope' , 'Moses','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Moses@test.com', 'participant'),
+('Hedley', 'Tonton' , 'Hedley','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Hedley@test.com', 'participant'),
+('Deborah', 'Wells' , 'Deborah','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Deborah@test.com', 'participant'),
+('Kay', 'Heils' , 'Kay','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Kay@test.com', 'participant'),
+('Dorothy', 'Joghny' , 'Dorothy','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Dorothy@test.com', 'participant'),
+('Doris', 'Baird' , 'Doris','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Doris@test.com', 'participant'),
+('Irene', 'Alaton' , 'Irene','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Irene@test.com', 'participant');
+
+
+
+INSERT INTO joutes.users (first_name, last_name ,username, password, email, role)
+VALUES ('Zorita', 'Ellis' , 'Zorita','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Zorita@Zorita.com', 'participant'),
+('Yen', 'Hale' , 'Yen','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Yen@Casey.com', 'participant'),
+('Madison', 'Marshall' , 'Madison','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Madison@Day.com', 'participant'),
+('Angela', 'Perry' , 'Angela','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Angela@test.com', 'participant'),
+('Michael', 'Woodward' , 'Michael','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Michael@test.com', 'participant'),
+('Karyn', 'Riddle' , 'Karyn','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Karyn@test.com', 'participant'),
+('Carol', 'Lang' , 'Carol','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Carol@test.com', 'participant'),
+('Malik', 'Padillia' , 'Malik','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Malik@test.com', 'participant'),
+('Maxine', 'Rowland' , 'Maxine','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Maxine@test.com', 'participant'),
+('Halee', 'Larson' , 'Halee','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Halee@test.com', 'participant');
+
+
+INSERT INTO joutes.users (first_name, last_name ,username, password, email, role)
+VALUES ('Tatyana', 'Rosario' , 'Tatyana','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Tatyana@Tatyana.com', 'participant'),
+('Latifah', 'Jenkiry' , 'Latifah','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Latifah@Casey.com', 'participant'),
+('Wynne', 'Rowload' , 'Wynne','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Wynne@Day.com', 'participant'),
+('Nola', 'Nola' , 'Nola','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Nola@test.com', 'participant'),
+('Nicole', 'WIlkys' , 'Nicole','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Nicole@test.com', 'participant'),
+('Sybil', 'Murray' , 'Sybil','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Sybil@test.com', 'participant'),
+('Cadman', 'Evans' , 'Cadman','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Cadman@test.com', 'participant'),
+('Xenos', 'Kramer' , 'Xenos','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Xenos@test.com', 'participant'),
+('Evan', 'Logan' , 'Evan','$2y$10$RsiBblUoNfis0/TAmWR3NuLQRUITxQbQmsaSAdCPyto1z4eUs4ZlW', 'Evan@test.com', 'participant');
+
